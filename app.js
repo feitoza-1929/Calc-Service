d3d2ffd144413079e04235e6fbb2f09f7ccea3c4
feitoza <41773200+feitoza-1929@users.noreply.github.com>
2022-02-04 13:36:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http")
 const url = require("url")
 
 const { getUrlData } = require("./ultils")
-const Todo = require('./controller')
+const Calculation = require('./controller')
 
 const PORT = process.env.PORT || 5000
 
@@ -12,7 +12,7 @@ const server = http.createServer( async(req, res) => {
     if (req.url.match(route)  && req.method === "GET") {
 
         const values = getUrlData(url.parse(req.url, true).path)
-        const result = await new Todo().getCalculation(values[0], values[1], values[2])
+        const result = await new Calculation().getCalculation(values[0], values[1], values[2])
 
         res.writeHead(200, {"Content-type": "aplication/json"})
         res.write(JSON.stringify({result : result}))
